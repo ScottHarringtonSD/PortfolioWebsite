d9f634e8e9b64338c6306703f9671168db83b0ca
Scott Harrington <scottharringtonsd@gmail.com>
2025-03-03 11:14:13 +0000

    readjusted card css

--- src/Projects/ProjectCard.tsx
+++ src/Projects/ProjectCard.tsx
@@ -8,23 +8,27 @@ interface ProjectCardProps {
 function ProjectCard(props: ProjectCardProps) {
   const { project } = props;
   return (
-    <div className="items-center">
-      <div className="mx-auto card max-w-sm border border-slate-500 rounded-lg shadow-lg dark:bg-slate-800 p-3 card-height">
-        <img
-          src={project.imageUrl}
-          alt={project.name}
-          className="rounded-t-lg h-1/2 "
-        ></img>
+    <div className="items-center justify-center">
+      <div className="mx-auto card max-w-sm border border-slate-500 rounded-lg shadow-lg dark:bg-slate-800 p-3 card-height items-center">
+        <div className="h-full w-full justify-center">
+          <img
+            src={project.imageUrl}
+            alt={project.name}
+            className="rounded-t-lg h-1/2 w-full"
+          ></img>
 
-        <div className="Opacity text-slate-400 max-h-6 pl-2 pt-1">
-          {" "}
-          Skills: {project.skills}
+          <div className="Opacity text-slate-400 max-h-6 pl-2 pt-1 w-full">
+            {" "}
+            Skills: {project.skills}
+          </div>
+          <h1 className="mb-2 text-2xl font-bold tracking-tight pl-2 pt-1 w-full">
+            {project.name}
+          </h1>
+
+          <section className="tracking-tight pb-3 pl-2 w-full">
+            {project.brief}
+          </section>
         </div>
-        <h1 className="mb-2 text-2xl font-bold tracking-tight pl-2 pt-1">
-          {project.name}
-        </h1>
-
-        <section className="tracking-tight pb-3 pl-2">{project.brief}</section>
       </div>
     </div>
   );
