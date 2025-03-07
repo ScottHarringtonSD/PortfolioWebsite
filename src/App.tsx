3a13ea6b295075c0c21cd748ce914d5f587dcbe9
Scott Harrington <scottharringtonsd@gmail.com>
2025-03-07 23:15:42 +0000

    get cool links background

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AboutMe from "./AboutMe";
@@ -11,27 +11,29 @@ import {
 } from "react-router-dom";
 import ApplicationsPage from "./Projects/ApplicationsPage";
 import GameDesignPage from "./Projects/GameDesignPage";
-// @ts-ignore
-import ParticleBackground from "react-particle-backgrounds";
+
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import type { Container, Engine } from "@tsparticles/engine";
+// import { loadAll } from "@/tsparticles/all"; // if you are going to use `loadAll`, install the "@tsparticles/all" package too.
+// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import { loadSlim } from "@tsparticles/slim";
 
 function App() {
-  const settings = {
-    particle: {
-      particleCount: 20,
-      color: "#94a3b8",
-      minSize: 20,
-      maxSize: 20,
-    },
-    velocity: {
-      minSpeed: 0.05,
-      maxSpeed: 1.5,
-    },
-    opacity: {
-      minOpacity: 0.2,
-      maxOpacity: 0.8,
-      opacityTransitionTime: 10000,
-    },
-    canvasFillSpace: true,
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container: Container | undefined): Promise<void> => {
+    return new Promise<void>((resolve) => {
+      console.log(container);
+      resolve();
+    });
   };
 
   return (
@@ -66,14 +68,6 @@ function App() {
       </header>
 
       <body className=" text-white min-h-screen text-font">
-        <ParticleBackground
-          settings={settings}
-          className="particles-bg bg-gradient-to-r from-slate-900 via-black  to-slate-900"
-        />
-        {/* <ParticleBackground
-          settings={settings}
-          className="particles-bg bg-white"
-        /> */}
         <div>
           <Routes>
             <Route path="/" element={<HomePage />} />
@@ -82,6 +76,80 @@ function App() {
             <Route path="/GameDesign" element={<GameDesignPage />} />
           </Routes>
         </div>
+        {init && (
+          <Particles
+            className="particles-bg"
+            id="tsparticles"
+            particlesLoaded={particlesLoaded}
+            options={{
+              background: {
+                color: {
+                  value: "#000",
+                },
+              },
+              fpsLimit: 120,
+              interactivity: {
+                events: {
+                  onClick: {
+                    enable: false,
+                    mode: "push",
+                  },
+                  onHover: {
+                    enable: false,
+                    mode: "repulse",
+                  },
+                },
+                modes: {
+                  push: {
+                    quantity: 4,
+                  },
+                  repulse: {
+                    distance: 100,
+                    duration: 10,
+                  },
+                },
+              },
+              particles: {
+                color: {
+                  value: "#475569",
+                },
+                links: {
+                  color: "#475569",
+                  distance: 200,
+                  enable: true,
+                  opacity: 0.5,
+                  width: 3,
+                },
+                move: {
+                  direction: "none",
+                  enable: true,
+                  outModes: {
+                    default: "bounce",
+                  },
+                  random: false,
+                  speed: 1,
+                  straight: false,
+                },
+                number: {
+                  density: {
+                    enable: true,
+                  },
+                  value: 100,
+                },
+                opacity: {
+                  value: 0.5,
+                },
+                shape: {
+                  type: "circle",
+                },
+                size: {
+                  value: { min: 1, max: 3 },
+                },
+              },
+              detectRetina: true,
+            }}
+          />
+        )}
       </body>
     </Router>
   );
