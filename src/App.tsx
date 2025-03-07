3e2b1f0ecc432e7802dc03d5b82f3925ed8856ab
Scott Harrington <scottharringtonsd@gmail.com>
2025-03-07 22:24:12 +0000

    First particle barckground

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import AboutMe from "./AboutMe";
@@ -11,12 +11,33 @@ import {
 } from "react-router-dom";
 import ApplicationsPage from "./Projects/ApplicationsPage";
 import GameDesignPage from "./Projects/GameDesignPage";
+// @ts-ignore
+import ParticleBackground from "react-particle-backgrounds";
 
 function App() {
+  const settings = {
+    particle: {
+      particleCount: 20,
+      color: "#94a3b8",
+      minSize: 20,
+      maxSize: 20,
+    },
+    velocity: {
+      minSpeed: 0.05,
+      maxSpeed: 1.5,
+    },
+    opacity: {
+      minOpacity: 0.2,
+      maxOpacity: 0.8,
+      opacityTransitionTime: 10000,
+    },
+    canvasFillSpace: true,
+  };
+
   return (
     <Router>
       <script src="../path/to/flowbite/dist/flowbite.min.js"></script>
-      <header className="bg-slate-800 text-white sticky top-0 z-10 text-font">
+      <header className="bg-gradient-to-r from-slate-700  via-slate-800 to-slate-700  text-white sticky top-0 z-10 text-font">
         <div className="max-w-5xl mx-auto flex p-4 items-center justify-between">
           <NavLink to="/">
             <h1 className="text-3xl flex text-left">Scott Harrington</h1>
@@ -44,7 +65,15 @@ function App() {
         </div>
       </header>
 
-      <body className="bg-black text-white min-h-screen text-font">
+      <body className=" text-white min-h-screen text-font">
+        <ParticleBackground
+          settings={settings}
+          className="particles-bg bg-gradient-to-r from-slate-900 via-black  to-slate-900"
+        />
+        {/* <ParticleBackground
+          settings={settings}
+          className="particles-bg bg-white"
+        /> */}
         <div>
           <Routes>
             <Route path="/" element={<HomePage />} />
--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function HomePage() {
   return (
-    <div className="max-w-4xl mx-auto flex justify-center p-6 pt-9 items-center">
-      <div className="text-center">
-        <h1 className="text-5xl font-bold">Scott Harrington</h1>
-        <p className="text-3xl text-slate-400 p-2">Full Stack Developer</p>
-        <p className="text-xl text-slate-400">
-          I am a young software professional currently working in consultancy,
-          with training in development, testing and application engineering.
-          Welcome to my portfolio!
-        </p>
+    <div className=" h-full w-full absolute ">
+      <div className=" max-w-4xl mx-auto flex justify-center p-6 pt-10 items-center mt-10">
+        <div className="text-center">
+          <h1 className="text-5xl text-slate-100">Scott Harrington</h1>
+          <p className="text-3xl text-slate-400 p-2">Full Stack Developer</p>
+          <p className="text-xl text-slate-400">
+            I am a young software professional currently working in consultancy,
+            with training in development, testing and application engineering.
+            Welcome to my portfolio!
+          </p>
+        </div>
       </div>
     </div>
   );
